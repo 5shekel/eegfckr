6374915e3e262cebebd74c7d7beace6e995af1eb
5shekel <yair99@gmail.com>
2016-02-18 14:55:35 +0800

    added ATTINY85_Tone_Freq_and_Note, from https://github.com/Comingle/ArduinoUtilityPrograms

---
+++ ATTINY85_Tone_Freq_and_Note/ATTINY85_Tone_Freq_and_Note.ino
@@ -0,0 +1,54 @@
+// code for sound and led output on an ATtiny85
+// Cleand up code from Kobakant
+// using leah buechley’s sound code, taken from: http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
+int speakerPin_0 = 0;
+int speakerPin_1 = 1;
+
+void setup()
+{
+  pinMode(speakerPin_0, OUTPUT);
+  pinMode(speakerPin_1, OUTPUT);
+}
+
+void loop() {
+  scale('E', speakerPin_0); //plays note C for half a second
+  scale('C', speakerPin_0); //plays note C for half a second
+  scale('G', speakerPin_0); //plays note C for half a second
+  scale('A', speakerPin_0 ); //plays note C for half a second
+}
+
+void scale (char note, int pin)
+{
+  if (note == 'C')
+    beep(pin, 2093, 500); //C: play the note C (C7 from the chart linked to above) for 500ms
+  if (note == 'D')
+    beep(pin, 2349, 500); //D
+  if (note == 'E')
+    beep(pin, 2637, 500); //E
+  if (note == 'F')
+    beep(pin, 2793, 500); //F
+  if (note == 'G')
+    beep(pin, 3136, 500); //G
+  if (note == 'A')
+    beep(pin, 3520, 500); //A
+  if (note == 'B')
+    beep(pin, 3951, 500); //B
+  if (note == 'H')
+    beep(pin, 4186, 500); //C
+}
+
+void beep (int pin, int frequencyInHertz, long timeInMilliseconds) // the sound producing function
+{
+  int x;
+  long delayAmount = (long)(1000000 / frequencyInHertz);
+  long loopTime = (long)((timeInMilliseconds * 1000) / (delayAmount * 2));
+  for (x = 0; x < loopTime; x++) {
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(delayAmount);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(delayAmount);
+    //break;
+  }
+
+}
+
--- eggfckr_ard/eggfckr_ard.ino
+++ eggfckr_ard/eggfckr_ard.ino
@@ -1,12 +1,12 @@
 /*
-Note1: some code from the adafriut gemma simple thermain example.
-Note2: The Arduino tone library does not work for the ATTiny85 on the
-Trinket and Gemma.  The beep function below is similar.  The beep code
-is adapted from Dr. Leah Buechley at
-http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
- */
+  Note1: some code from the adafriut gemma simple thermain example.
+  Note2: The Arduino tone library does not work for the ATTiny85 on the
+  Trinket and Gemma.  The beep function below is similar.  The beep code
+  is adapted from Dr. Leah Buechley at
+  http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
+*/
 
-#define SPEAKER   0    // Speaker on GPIO #0
+#define speaker_pin   0    // speaker_pin on GPIO #0
 #define SCALE     2.0  // You can change this to change the tone scale
 
 #define led1 2
@@ -17,45 +17,68 @@ http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
 int logy = 5;
 
 void setup() {
-  pinMode(SPEAKER,OUTPUT);
-  
+  pinMode(speaker_pin, OUTPUT);
+
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(pot, INPUT);
-  
+
 }
 void loop() {
-  
+
   int input = analogRead(pot);  //between 4-32Hz blinks, 4 is 250ms, 32 is 31ms
   int freqLight = map(input, 100, 1024, 31, 250);
-  freqLight = constrain(freqLight, 31,250);
-
-  int freqAudio=220+(int)(input*SCALE); // change the voltage read to a frequency
-                             // you can change the values to scale
-                             // your frequency range  
-  beep(SPEAKER,freqAudio,400);    // output the tone to digital pin 0
-                             // you can change 400 to different times}
+  freqLight = constrain(freqLight, 31, 250);
 
   digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);   
-  delay(hightimes); 
+  digitalWrite(led2, LOW);
+  delay(hightimes);
   digitalWrite(led1, HIGH);
   digitalWrite(led2, HIGH);
-  delay(freqLight-hightimes);
+  delay(freqLight - hightimes);
+
+  /*
+    int freqAudio=220+(int)(input*SCALE); // change the voltage read to a frequency
+                             // you can change the values to scale
+                             // your frequency range
+    beep(speaker_pin,freqAudio,600);    // output the tone to digital pin 0
+                             // you can change 400 to different times}
+  */
 }
 
- 
+
 // the sound producing function
-void beep (unsigned char speakerPin, int frequencyInHertz, long timeInMilliseconds)
-{   // http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
-          int x;   
-          long delayAmount = (long)(1000000/frequencyInHertz);
-          long loopTime = (long)((timeInMilliseconds*1000)/(delayAmount*2));
-          for (x=0;x<loopTime;x++)   
-          {  
-              digitalWrite(speakerPin,HIGH);
-              delayMicroseconds(delayAmount);
-              digitalWrite(speakerPin,LOW);
-              delayMicroseconds(delayAmount);
-          }  
+void beep (unsigned char speaker_pinPin, int frequencyInHertz, long timeInMilliseconds)
+{ // http://web.media.mit.edu/~leah/LilyPad/07_sound_code.html
+  int x;
+  long delayAmount = (long)(1000000 / frequencyInHertz);
+  long loopTime = (long)((timeInMilliseconds * 1000) / (delayAmount * 2));
+  for (x = 0; x < loopTime; x++)
+  {
+    digitalWrite(speaker_pinPin, HIGH);
+    delayMicroseconds(delayAmount);
+    digitalWrite(speaker_pinPin, LOW);
+    delayMicroseconds(delayAmount);
+  }
 }
+
+void scale (char note)
+{
+  if (note == 'C')
+    beep(speaker_pin, 2093, 500); //C: play the note C (C7 from the chart linked to above) for 500ms
+  if (note == 'D')
+    beep(speaker_pin, 2349, 500); //D
+  if (note == 'E')
+    beep(speaker_pin, 2637, 500); //E
+  if (note == 'F')
+    beep(speaker_pin, 2793, 500); //F
+  if (note == 'G')
+    beep(speaker_pin, 3136, 500); //G
+  if (note == 'A')
+    beep(speaker_pin, 3520, 500); //A
+  if (note == 'B')
+    beep(speaker_pin, 3951, 500); //B
+  if (note == 'H')
+    beep(speaker_pin, 4186, 500); //C
+}
+
